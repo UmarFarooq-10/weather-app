73f6628fc26b27e986d3ddf0c840408ac5629a0b
“[Umar <uf636630@gmail.com>
2023-07-21 19:16:36 +0500

    weather application built using weather apis

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "44389e3726163572467a0c0955af6d2a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
